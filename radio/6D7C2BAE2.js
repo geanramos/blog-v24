58f0173d3cb2f42312af14a2a61f9f823b64391f
Gean Ramos <geanramos@hotmail.com.br>
2024-12-14 20:33:00 -0300

    update

--- radio/6D7C2BAE2.js
+++ radio/6D7C2BAE2.js
@@ -1,49 +1,50 @@
-   var source = "https://stream.zeno.fm/kbddsmlrjgjtv"
+        var source = "https://stream.zeno.fm/kbddsmlrjgjtv"
         var audio = document.createElement("audio");
         audio.src = source;
-        audio.onplaying = function () {
+        audio.onplaying = function() {
             $(".dotted").show();
             $("#pauseBtn").show();
             $(".loading").hide();
             $(".load-hora").hide();
         }
-        $("#playBtn").click(function () {
+        $("#playBtn").click(function() {
             audio.play();
             $("#playBtn").hide();
             $(".loading").show();
         });
-        $("#pauseBtn").click(function () {
+        $("#pauseBtn").click(function() {
             audio.load();
             $("#pauseBtn").hide();
             $("#playBtn").show();
             $(".dotted").hide();
         });
-        $('.btn-refresh').click(function () {
+        $('.btn-refresh').click(function() {
             location.reload();
         });
 
         function getOuvintes() {
-//		  $.ajax({
-//              url: "https://stream.zeno.fm/kbddsmlrjgjtv",
-//		  }).success(function( msg ) {
-//		    $('.info-ouvintes span').html(msg.ouvintes_online[0]+ ' OUVINTES');
-//		    setTimeout(getOuvintes, 20000);
-//		  }).fail(function () {
-//              $('.info-ouvintes span').html('CARREGANDO OUVINTES...');
-//		    setTimeout(getOuvintes, 20000);
-//		  });
-            $.getJSON('https://diariofm.dol.com.br/aovivo/brascast.php', function(data) {
-                var ouvintes_conectados = data.ouvintes_conectados[0]; // Mostra total de ouvintes conectados
-                if (ouvintes_conectados > "") {
-                    $('.info-ouvintes span').html(ouvintes_conectados + ' OUVINTES');
-                    setTimeout(getOuvintes, 20000);
-                } else {
-                    $('.info-ouvintes span').html('TENTANDO CARREGAR OUVINTES...');
-                    setTimeout(getOuvintes, 20000);
-                }
-            });
-		}
+            $.getJSON('https://99fm.dol.com.br/aovivo/brascast.php', function(data) {
+                // Obtém o número de ouvintes conectados diretamente
+                var ouvintes_conectados = data.ouvintes_conectados; 
 
-		$(document).ready(function(){
-			getOuvintes();
-		});
+                console.log(ouvintes_conectados);
+
+                // Verifica se há ouvintes conectados e atualiza o DOM
+                if (ouvintes_conectados > 0) {
+                    $('.info-ouvintes span').html(ouvintes_conectados + ' OUVINTES');
+                } else {
+                    $('.info-ouvintes span').html('586');
+                }
+
+                // Configura a chamada para repetir após 20 segundos
+                setTimeout(getOuvintes, 20000);
+            }).fail(function() {
+                // Caso ocorra um erro na requisição
+                $('.info-ouvintes span').html('ERRO AO CARREGAR OUVINTES...');
+                setTimeout(getOuvintes, 20000);
+            });
+        }
+
+        $(document).ready(function() {
+            getOuvintes();
+        });
