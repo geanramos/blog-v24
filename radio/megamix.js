74540296a4ec80c6a304104f78af665bf9b441be
Gean Ramos <geanramos@hotmail.com.br>
2025-01-11 03:37:45 -0300

    Update megamix.js

--- radio/megamix.js
+++ radio/megamix.js
@@ -23,7 +23,7 @@
         });
 
         function getOuvintes() {
-            $.getJSON('https://diariofm.dol.com.br/aovivo/brascast.php', function(data) {
+            $.getJSON('https://99fm.dol.com.br/aovivo/brascast.php', function(data) {
                 // Obtém o número de ouvintes conectados diretamente
                 var ouvintes_conectados = data.ouvintes_conectados; 
 
@@ -31,7 +31,7 @@
 
                 // Verifica se há ouvintes conectados e atualiza o DOM
                 if (ouvintes_conectados > 0) {
-                    $('.info-ouvintes span').html('1' + ouvintes_conectados + ' OUVINTES');
+                    $('.info-ouvintes span').html('3' + ouvintes_conectados + ' OUVINTES');
                 } else {
                     $('.info-ouvintes span').html('TENTANDO CARREGAR OUVINTES...');
                 }
