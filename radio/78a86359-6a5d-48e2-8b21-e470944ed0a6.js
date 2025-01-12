98080e2a4f5ff980eab993bee3da204f8c92a9d1
Gean Ramos <geanramos@hotmail.com.br>
2025-01-12 19:49:37 -0300

    update

--- radio/78a86359-6a5d-48e2-8b21-e470944ed0a6.js
+++ radio/78a86359-6a5d-48e2-8b21-e470944ed0a6.js
@@ -1,19 +1,35 @@
     // Array de URLs das imagens
     const imagens = [
-      "https://i1.wp.com/radiomixfm.com.br/wp-content/uploads/2021/08/botao-app.jpg",
-      "https://i1.wp.com/radiomixfm.com.br/wp-content/uploads/2021/08/botao-youtube-1.jpg",
-      "https://i1.wp.com/aovivo.radiomixfm.com.br/imagens/bg-escuro-player.jpg?resize=360,90",
+      "https://geanramos.com.br/radio/img/fno-dinheiro.jpg",
       "https://geanramos.com.br/img/fno.jpg",
-      "https://i1.wp.com/tpc.googlesyndication.com/simgad/1182827443883241536?resize=360,90"
+      "https://i1.wp.com/tpc.googlesyndication.com/simgad/1182827443883241536?resize=360,90",
+	  "https://geanramos.com.br/radio/img/features1.jpg",
+"https://geanramos.com.br/radio/img/features2.jpg",
+"https://geanramos.com.br/radio/img/features3.jpg",
+"https://geanramos.com.br/radio/img/features4.jpg",
+"https://geanramos.com.br/radio/img/features5.jpg",
+"https://geanramos.com.br/radio/img/features6.jpg",
+"https://geanramos.com.br/radio/img/features7.jpg",
+"https://geanramos.com.br/radio/img/features8.jpg",
+"https://geanramos.com.br/radio/img/features9.jpg",
+"https://geanramos.com.br/radio/img/features10.jpg",
     ];
 
     // Array de URLs dos links correspondentes
     const links = [
-      "https://goo.gl/X1zq32",
-      "https://www.youtube.com/radiomixfm?sub_confirmation=1",
       "https://geanramos.com.br/ig/",
       "https://geanramos.com.br/ig/",
-      "https://www.change.org/p/manifesto-em-defesa-dos-o%C3%A1sis-de-not%C3%ADcias?utm_medium=radio&utm_source=geanramos.com.br"
+      "https://www.change.org/p/manifesto-em-defesa-dos-o%C3%A1sis-de-not%C3%ADcias?utm_medium=radio&utm_source=geanramos.com.br",
+	  "https://geanramos.com.br/radio/bda.html",
+"https://geanramos.com.br/radio/pop.html",
+"https://geanramos.com.br/radio/sertanejo.html",
+"https://geanramos.com.br/radio/hits-brasil.html",
+"https://geanramos.com.br/radio/gospel.html",
+"https://geanramos.com.br/radio/",
+"https://geanramos.com.br/radio/libcast.html",
+"https://geanramos.com.br/radio/libbel.html",
+"https://geanramos.com.br/radio/libmusic.html",
+"https://geanramos.com.br/radio/mixfm.html",
     ];
 
     // Função para exibir um footer-banner aleatório
