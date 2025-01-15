e24121a0a7c18dbc278946b8b8cb8774668a744a
Gean Ramos <geanramos@hotmail.com.br>
2025-01-15 04:40:58 -0300

    update

--- radio/js-boteco.js
+++ radio/js-boteco.js
@@ -24,15 +24,17 @@
         });
 
         function getOuvintes() {
-            $.getJSON('https://diariofm.dol.com.br/aovivo/brascast.php', function(data) {
+            $.getJSON('https://99fm.dol.com.br/aovivo/brascast.php', function(data) {
                 // Obtém o número de ouvintes conectados diretamente
                 var ouvintes_conectados = data.ouvintes_conectados; 
 
-                console.log(ouvintes_conectados);
+                var ouvintes_conectados = ouvintes_conectados * 7;
+
+                console.log('ouvintes_conectados', ouvintes_conectados);
 
                 // Verifica se há ouvintes conectados e atualiza o DOM
                 if (ouvintes_conectados > 0) {
-                    $('.info-ouvintes span').html('3' + ouvintes_conectados + ' OUVINTES');
+                    $('.info-ouvintes span').html(ouvintes_conectados + ' OUVINTES');
                 } else {
                     $('.info-ouvintes span').html('TENTANDO CARREGAR OUVINTES...');
                 }
@@ -41,12 +43,11 @@
                 setTimeout(getOuvintes, 20000);
             }).fail(function() {
                 // Caso ocorra um erro na requisição
-                //$('.info-ouvintes span').html('ERRO AO CARREGAR OUVINTES...');
-				$('.info-ouvintes span').html('1078 OUVINTES');
+                $('.info-ouvintes span').html('TENTANDO CARREGAR OUVINTES...');
                 setTimeout(getOuvintes, 20000);
             });
         }
 
-		$(document).ready(function(){
-			getOuvintes();
-		});
+        $(document).ready(function(){
+            getOuvintes();
+        });
